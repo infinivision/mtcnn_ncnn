2c06ffc4cadd7a6eb22ffcd4dd7b84b0ac6a01ca
Hongtao Zhang <553780043@qq.com>
2018-05-14 16:29:03 +0800

    remove unused code

--- include/mtcnn.h
+++ include/mtcnn.h
@@ -55,22 +55,6 @@ private:
 	const float threshold[3] = { 0.8f, 0.8f, 0.6f };
 	int minsize = 80;
 	const float pre_facetor = 0.709f;
-
-    // void generateBbox(ncnn::Mat score, ncnn::Mat location, vector<Bbox>& boundingBox_, vector<orderScore>& bboxScore_, float scale);
-    // void nms(vector<Bbox> &boundingBox_, std::vector<orderScore> &bboxScore_, const float overlap_threshold, string modelname="Union");
-    // void refineAndSquareBbox(vector<Bbox> &vecBbox, const int &height, const int &width);
-
-    // ncnn::Net pnet_, rnet_, onet_;
-    // ncnn::Mat img;
-
-    // const float nms_threshold[3] = {0.5, 0.7, 0.7};
-    // const float threshold[3] = {0.7, 0.6, 0.7};
-    // const float mean_vals[3] = {127.5, 127.5, 127.5};
-    // const float norm_vals[3] = {0.0078125, 0.0078125, 0.0078125};
-
-    // std::vector<Bbox> firstBbox_, secondBbox_,thirdBbox_;
-    // std::vector<orderScore> firstOrderScore_, secondBboxScore_, thirdBboxScore_;
-    // int img_w, img_h;
 };
 
 
